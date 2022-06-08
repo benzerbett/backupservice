baf008fb480cf9e513636cfc150bae5c7d67cb79
Benzer <benzerbett@gmail.com>
2022-06-08 16:55:59 +0300

    Commit 001

---
+++ README.md
@@ -0,0 +1,33 @@
+# BackupService
+
+This is a simple solution to push the contents of a specified folder to respective Minio buckets with similar names.
+
+I used the latest version of Golang (1.18 as of writing this), so you may need to refactor accordingly.
+
+### Disclosure
+This repo uses some experimental features in Go 1.18. This is the [slices](https://pkg.go.dev/golang.org/x/exp/slices) package using generics on 1.18. For further info, please refer to [https://pkg.go.dev/golang.org/x/exp/slices](https://pkg.go.dev/golang.org/x/exp/slices)
+
+### Set-up
+
+- Clone this repo
+    ```bash
+      git clone https://github.com/benzerbett/backupservice
+    ```
+- Install dependencies
+    ```bash
+        cd backupservice
+        go get
+    ```
+- Set up environment variables. The variables are:
+  * MINIO_ENDPOINT
+  * MINIO_ACCESSKEY
+  * MINIO_SECRET
+  * BACKUP_FILES_DIR
+  ```bash
+      nano .env
+  ```
+- Build & deploy
+ ```bash
+    go build -o backupservice
+```
+To deploy, you can schedule this as a cron job or otherwise, depending on your environment.
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"golang.org/x/exp/slices"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+func main() {
+	ctx := context.Background()
+	// Load environment variables
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatalln(err)
+	}
+	minioUrl := os.Getenv("MINIO_ENDPOINT")
+	backupsDir := os.Getenv("BACKUP_FILES_DIR")
+	minioKey := os.Getenv("MINIO_ACCESSKEY")
+	minioSecret := os.Getenv("MINIO_SECRET")
+	minioSSL := false
+	fmt.Println("URL: ",minioUrl)
+	fmt.Println("ACCESS_KEY: ",minioKey)
+	fmt.Println("SECRET: ", minioSecret)
+
+	// Set up minio
+	endpoint := minioUrl			// "play.min.io"
+	accessKeyID := minioKey 		// "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := minioSecret	// "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	useSSL := minioSSL				// true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//log.Printf("%#v\n", minioClient)
+
+	// list buckets
+	buckets, err := minioClient.ListBuckets(context.Background())
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	// get files
+	dirFs, err := ioutil.ReadDir(backupsDir)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("\n\n")
+
+	// loop through files
+	for i, f := range dirFs {
+		// for each file, find a bucket with a corresponding name
+		fileName := strings.ToLower( strings.Split(f.Name(), ".sql")[0] )
+		fileBucketIndex := slices.IndexFunc(buckets, func(b minio.BucketInfo) bool { return fileName == strings.ToLower(strings.Split(b.Name, "-backups")[0]) })
+		if fileBucketIndex != -1 {
+			fileBucket := buckets[fileBucketIndex]
+			fmt.Println( fmt.Sprint(i+1)+") File: "+fmt.Sprint(f.Name())+", Bucket: "+fileBucket.Name)
+			fopen, err := os.Open(backupsDir+"/"+f.Name())
+			fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+			if err != nil {
+				log.Fatalln("Could not open file "+f.Name()+". ", err)
+			}
+			fileType, err := GetFileContentType(fopen)
+			if err != nil {
+				log.Fatalln("Could not read file type.", err)
+			}
+			info, err := minioClient.FPutObject(ctx, fileBucket.Name, f.Name(), backupsDir+"/"+f.Name(), minio.PutObjectOptions{ContentType: fileType})
+			if err != nil {
+				log.Fatalln(err)
+			}
+			log.Printf("Successfully uploaded %s of size %d\n", f.Name(), info.Size)
+		}else{
+			fmt.Println( "File: "+fmt.Sprint(f.Name()) + ", Bucket not found")
+		}
+	}
+
+}
+func GetFileContentType(ouput *os.File) (string, error) {
+
+	// to sniff the content type only the first
+	// 512 bytes are used.
+
+	buf := make([]byte, 512)
+
+	_, err := ouput.Read(buf)
+
+	if err != nil {
+		return "", err
+	}
+
+	// the function that actually does the trick
+	contentType := http.DetectContentType(buf)
+
+	return contentType, nil
+}
